eeec2ddd15df2abe7d54712fad712984e152ee5e
Piotr Kościelniak <piotrek.koscielniak@gmail.com>
2015-12-28 19:23:02 +0100

    parser added minus and multidigit integers

--- simpleCalculator.go
+++ simpleCalculator.go
@@ -29,58 +29,99 @@ type Interpreter struct {
 	Text         string
 	Pos          int
 	CurrentToken *Token
+	CurrentChar  *rune
+}
+
+func NewInterpreter(text string) *Interpreter {
+	first := rune(text[0])
+	i := &Interpreter{
+		Text: text,
+		Pos: 0,
+		CurrentToken: nil,
+		CurrentChar: &first,
+	}
+	return i
 }
 
 func (i *Interpreter) Error(msg string) {
 	panic(msg)
 }
 
-func (i *Interpreter) GetNextToken() *Token {
-	text := i.Text
-	var token *Token
-	if i.Pos > len(text) - 1 {
-		return &Token{
-			Type:EOF, Value: nil}
+func (i *Interpreter) advance() {
+	i.Pos += 1
+	if i.Pos > len(i.Text) - 1 {
+		i.CurrentChar = nil
+	} else {
+		r := rune(i.Text[i.Pos])
+		i.CurrentChar = &r
 	}
-	currentChar := rune(text[i.Pos])
+}
 
-	if unicode.IsDigit(currentChar) {
-		val, err := strconv.Atoi(string(currentChar))
-		if err != nil {
-			i.Error("GetNextToken. Not Digit")
-		}
-		token = &Token{
-			Type: INTEGER,
-			Value:val,
-		}
-		i.Pos += 1
-		return token
+func (i *Interpreter) skipWhitespace() {
+	for i.CurrentChar != nil && unicode.IsSpace(*i.CurrentChar) {
+		i.advance()
 	}
+}
 
-	if currentChar == '+' {
-		token = &Token{Type:PLUS, Value:currentChar}
-		i.Pos += 1
-		return token
+func (i *Interpreter) integer() int {
+	result := ""
+	for i.CurrentChar != nil && unicode.IsDigit(*i.CurrentChar) {
+		result += string(*i.CurrentChar)
+		i.advance()
 	}
-	i.Error("GetNextToken. Not match. Error parsing input")
-	return nil
+	res, err := strconv.Atoi(result)
+	if err != nil {
+		i.Error(fmt.Sprintf("Integer %v", err))
+	}
+	return res
+}
+
+func (i *Interpreter) getNextToken() *Token {
+
+	for i.CurrentChar != nil {
+		if unicode.IsSpace(*i.CurrentChar) {
+			i.skipWhitespace()
+			continue
+		}
+		if unicode.IsDigit(*i.CurrentChar) {
+			return &Token{
+				Type: INTEGER,
+				Value:i.integer(),
+			}
+		}
+		if *i.CurrentChar == '+' {
+			i.advance()
+			return &Token{Type:PLUS, Value:"+"}
+		}
+		if *i.CurrentChar == '-' {
+			i.advance()
+			return &Token{Type:MINUS, Value:"-"}
+		}
+		i.Error("getNextToken Not match")
+	}
+	return &Token{
+		Type:EOF, Value: nil}
 }
 
 func (i *Interpreter) Eat(tType TokenType) {
 	if i.CurrentToken.Type == tType {
-		i.CurrentToken = i.GetNextToken()
+		i.CurrentToken = i.getNextToken()
 	} else {
 		i.Error("Eat: Error parsing input")
 	}
 }
 
 func (i *Interpreter) Expr() int {
-	i.CurrentToken = i.GetNextToken()
+	i.CurrentToken = i.getNextToken()
 
 	left := i.CurrentToken
 	i.Eat(INTEGER)
-	_ = i.CurrentToken
-	i.Eat(PLUS)
+	op := i.CurrentToken
+	if op.Type == PLUS {
+		i.Eat(PLUS)
+	} else {
+		i.Eat(MINUS)
+	}
 
 	right := i.CurrentToken
 	i.Eat(INTEGER)
@@ -88,7 +129,13 @@ func (i *Interpreter) Expr() int {
 	lVal := left.Value.(int);
 	rVal := right.Value.(int);
 
-	return lVal + rVal
+	var result int
+	if op.Type == PLUS {
+		result = lVal + rVal
+	} else {
+		result = lVal - rVal
+	}
+	return result
 }
 
 func main() {
@@ -100,9 +147,7 @@ func main() {
 	//	text  := "2+3"
 	fmt.Println(text)
 
-	inter := Interpreter{
-		Text: text,
-	}
+	inter := NewInterpreter(text)
 	res := inter.Expr()
 	fmt.Printf("Result: %v\n", res)
 
