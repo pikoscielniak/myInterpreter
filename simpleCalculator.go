2b7eae65669de42fde2bbb9ff5d0a145fa662035
Piotr Kościelniak <piotrek.koscielniak@gmail.com>
2015-12-30 21:14:58 +0100

    4 operations and its precedents

--- simpleCalculator.go
+++ simpleCalculator.go
@@ -13,6 +13,8 @@ const (
 	INTEGER TokenType = "INTEGER"
 	PLUS TokenType = "PLUS"
 	MINUS TokenType = "MINUS"
+	MUL TokenType = "MUL"
+	DIV TokenType = "DIV"
 	EOF TokenType = "EOF"
 )
 
@@ -25,29 +27,27 @@ func (t Token)String() string {
 	return fmt.Sprintf("Token({%v}, {%v})", t.Type, t.Value)
 }
 
-type Interpreter struct {
-	Text         string
-	Pos          int
-	CurrentToken *Token
-	CurrentChar  *rune
+type Lexer struct {
+	Text        string
+	Pos         int
+	CurrentChar *rune
 }
 
-func NewInterpreter(text string) *Interpreter {
+func NewLexer(text string) *Lexer {
 	first := rune(text[0])
-	i := &Interpreter{
+	i := &Lexer{
 		Text: text,
 		Pos: 0,
-		CurrentToken: nil,
 		CurrentChar: &first,
 	}
 	return i
 }
 
-func (i *Interpreter) Error(msg string) {
-	panic(msg)
+func (i *Lexer) Error(msg string) {
+	panic("LEXER " + msg)
 }
 
-func (i *Interpreter) advance() {
+func (i *Lexer) advance() {
 	i.Pos += 1
 	if i.Pos > len(i.Text) - 1 {
 		i.CurrentChar = nil
@@ -57,13 +57,13 @@ func (i *Interpreter) advance() {
 	}
 }
 
-func (i *Interpreter) skipWhitespace() {
+func (i *Lexer) skipWhitespace() {
 	for i.CurrentChar != nil && unicode.IsSpace(*i.CurrentChar) {
 		i.advance()
 	}
 }
 
-func (i *Interpreter) integer() int {
+func (i *Lexer) integer() int {
 	result := ""
 	for i.CurrentChar != nil && unicode.IsDigit(*i.CurrentChar) {
 		result += string(*i.CurrentChar)
@@ -76,7 +76,7 @@ func (i *Interpreter) integer() int {
 	return res
 }
 
-func (i *Interpreter) getNextToken() *Token {
+func (i *Lexer) getNextToken() *Token {
 
 	for i.CurrentChar != nil {
 		if unicode.IsSpace(*i.CurrentChar) {
@@ -97,46 +97,92 @@ func (i *Interpreter) getNextToken() *Token {
 			i.advance()
 			return &Token{Type:MINUS, Value:"-"}
 		}
+		if *i.CurrentChar == '*' {
+			i.advance()
+			return &Token{Type:MUL, Value:"*"}
+		}
+		if *i.CurrentChar == '/' {
+			i.advance()
+			return &Token{Type:DIV, Value:"/"}
+		}
 		i.Error("getNextToken Not match")
 	}
 	return &Token{
 		Type:EOF, Value: nil}
 }
 
+type Interpreter struct {
+	lexer        *Lexer
+	CurrentToken *Token
+}
+
+func NewInterpreter(l *Lexer) *Interpreter {
+	i := &Interpreter{
+		lexer: l,
+	}
+	i.CurrentToken = i.lexer.getNextToken()
+	return i
+}
+
+func (i *Interpreter) Error(msg string) {
+	panic("INTERPRETER " + msg)
+}
+
 func (i *Interpreter) Eat(tType TokenType) {
 	if i.CurrentToken.Type == tType {
-		i.CurrentToken = i.getNextToken()
+		i.CurrentToken = i.lexer.getNextToken()
 	} else {
 		i.Error("Eat: Error parsing input")
 	}
 }
 
-func (i *Interpreter) Expr() int {
-	i.CurrentToken = i.getNextToken()
+func (i *Interpreter) factor() int {
+	t := i.CurrentToken
+	i.Eat(INTEGER)
+	return t.Value.(int)
+}
 
+
+func (i *Interpreter) term() int {
+	result := i.factor()
+
+	for i.CurrentToken.Type == MUL ||
+	i.CurrentToken.Type == DIV {
+		token := i.CurrentToken
+		if token.Type == MUL {
+			i.Eat(MUL)
+			result = result * i.factor()
+		} else if token.Type == DIV {
+			i.Eat(DIV)
+			result = result / i.factor()
+		}
+	}
+	return result
+}
+
+func (i *Interpreter) Expr() int {
 	result := i.term()
 
 	for i.CurrentToken.Type == PLUS ||
 	i.CurrentToken.Type == MINUS {
 		token := i.CurrentToken
-
 		if token.Type == PLUS {
 			i.Eat(PLUS)
 			result = result + i.term()
-		} else if token.Type == MINUS{
+		} else if token.Type == MINUS {
 			i.Eat(MINUS)
 			result = result - i.term()
 		}
 	}
-
 	return result
 }
 
-func (i *Interpreter) term() int {
-	t := i.CurrentToken
-	i.Eat(INTEGER)
-	return t.Value.(int)
-}
+//
+//func (i *Interpreter) term() int {
+//	t := i.CurrentToken
+//	i.Eat(INTEGER)
+//	return t.Value.(int)
+//}
 
 func main() {
 
@@ -147,7 +193,8 @@ func main() {
 	//	text  := "2+3"
 	fmt.Println(text)
 
-	inter := NewInterpreter(text)
+	lex := NewLexer(text)
+	inter := NewInterpreter(lex)
 	res := inter.Expr()
 	fmt.Printf("Result: %v\n", res)
 
